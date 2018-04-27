e7af5338338edef102d2c7b0b0a9949afc72ac35
zarddy <foreverlzw@hotmail.com>
2018-04-27 14:06:27 +0800

    调整列表页

--- src/pages/cat/List.vue
+++ src/pages/cat/List.vue
@@ -33,6 +33,16 @@
 
     </el-table>
 
+        <div class="pagination">
+            <el-pagination
+                :current-page.sync="currentPage"
+                :page-size="pageSize"
+                layout="prev, pager, next, total, jumper"
+                background
+                :total="total"
+                @current-change="listCats">
+            </el-pagination>
+        </div>
 
 
   </div>
@@ -43,29 +53,32 @@ export default {
   name: 'CatList',
   data () {
     return {
-      mPage: 1,
+      currentPage: 1, // 当前页码
+      pageSize: 30,
+      total: 0,
       msg: 'cat list',
       result_list: []
     }
   },
   mounted() {
-    this.listCats(this.mPage);
+    this.listCats();
   },
   methods: {
-    listCats(page) {
+    listCats() {
       let _this = this;
-      let params = new FormData();
-                    params.append("state", 1);
-                    params.append("page", page);
-                    // 以post形式提交
-                    this.$http.post('/cat/list',params).then(
-                        data=> {
-                            if (data.status == 200) {
-                                //  数据获取成功
-                                _this.result_list = data.data;
-                            }
-                        }
-                    )
+      // 以post形式提交
+      // this.$http.post('/cat/list',params).then(
+      this.$http.post('/cat/list', {state: 1, page: this.currentPage, pageSize: this.pageSize}).then(
+        data=> {
+          if (data.status == 200) {
+            //  数据获取成功
+            _this.result_list = data.data;
+            _this.total = data.total;
+          } else {
+            console.log("数据获取失败 . . " + data.msg);
+          }
+        }
+      )
     }
   }
 }
